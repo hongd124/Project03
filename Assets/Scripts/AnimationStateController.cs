afbe2d114cb917441fcaa88acae190a2b3f591f1
Dylan Hong <hongd124@gmail.com>
2021-11-30 23:51:14 -0600

    animation configure

---
+++ Assets/Scripts/AnimationStateController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStateController : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool forwardPressed = Input.GetKey("w");
+        bool rightPressed = Input.GetKey("d");
+        bool leftPressed = Input.GetKey("a");
+        bool backPressed = Input.GetKey("s");
+
+        // forward movement animation
+        if (forwardPressed)
+        {
+            animator.SetBool("isRunning", true);
+        }
+
+        if (!forwardPressed)
+        {
+            animator.SetBool("isRunning", false);
+        }
+    }
+}
--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -7,19 +7,26 @@ public class ThirdPersonMovement : MonoBehaviour
     public CharacterController _controller;
     public Transform cam;
 
+    Animator anim;
+
     public float _speed = 6f;
 
     public float _turnTime = 0.1f;
     float turnSmoothVelocity;
 
+    void Start()
+    {
+        
+    }
+
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
-        if(direction.magnitude >= 0.1f)
+        
+        if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, _turnTime);
